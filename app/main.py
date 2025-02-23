2306ac905ed57c554ac54b549ee41018a342e4e0
Justin <128965379+JustinChaney2023@users.noreply.github.com>
2025-02-22 19:37:01 -0900

    added echo builtin

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 import sys
-
+import os
 
 def main():
-
     while True:
         sys.stdout.write("$ ")
 
         # Wait for user input
-        command = input()
-        if command == "exit 0":
+        command = input().strip()
+        if command.startswith("exit"):
             break
+        elif command.startswith("echo"):
+            echo(command[5:].strip())
         else:
             print(f"{command}: command not found")
 
+def echo(text):
+    print(text)
 
 if __name__ == "__main__":
     main()
