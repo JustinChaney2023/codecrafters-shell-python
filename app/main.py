cc4b1ec4d79a84b90059256397a7e773ce783cf2
Justin <128965379+JustinChaney2023@users.noreply.github.com>
2025-02-22 20:11:53 -0900

    added type builtin: builtins

--- app/main.py
+++ app/main.py
@@ -8,14 +8,31 @@ def main():
         # Wait for user input
         command = input().strip()
         if command.startswith("exit"):
-            break
+            exit(command[5:].strip())
         elif command.startswith("echo"):
             echo(command[5:].strip())
+        elif command.startswith("type"):
+            print(type(command[5:].strip()))
         else:
             print(f"{command}: command not found")
 
-def echo(text):
+def exit(code):
+    try:
+        sys.exit(int(code)) # Converts string to integer to take exit code
+    except ValueError:
+        sys.exit(0)         # Defaults to 0 if no integer is provided
+
+def echo(text):             # Prints text taken after echo command
     print(text)
+
+def type(command):          # Gives command type
+    builtins = ["echo", "exit"]
+
+    if command in builtins:
+        return f"{command} is a shell builtin"
+    else:
+        return f"{command}: not found"
+
 
 if __name__ == "__main__":
     main()
